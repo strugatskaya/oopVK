f470034374aa62a1d80f21a25a0be371a803fe2d
Juliya Strugatskaya <strugatskaya.um@gmail.com>
2021-06-24 23:54:34 +0300

    Post Manager

--- src/main/java/ru/netology/domain/GeoInfo.java
+++ src/main/java/ru/netology/domain/GeoInfo.java
@@ -5,5 +5,5 @@ public class GeoInfo {
     private String type;
     private String coordinates;
     private Place place;
-    // + get/set на все поля
+    // getters/setters
 }
---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,18 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+
+    private Post[] post;
+
+    public Post[] searchPost(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+//        TODO: add logic
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+//        TODO: add logic
+    }
+}
+
